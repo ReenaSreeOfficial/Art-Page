e84a51eb2e75f54e535043997c8b2087c84a0c33
ReenaSreeOfficial <reenasreenutenkiofficial@gmail.com>
2025-07-15 11:48:37 +0530

    This is my simple Frontend for Art page
    need some suggestions on this any changes i want to do or anything

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+        <h4>
+          Hello World
+        </h4>
+        
+  );
+}
+
+export default App;
